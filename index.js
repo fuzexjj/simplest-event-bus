03ddb67918a4dbd3ecca41a842ed2cda7af9f8cb
Kirill Koshcheev <kosheevkirill@gmail.com>
2021-05-02 01:50:16 +0200

    Start Classes implementation

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-console.log('Hello, world')
+class Mailer {
+    sendEmail() {
+        console.log('Email sent to the user')
+    }
+}
+
+class Order {
+    constructor(name = 'Unselected') {
+        this.itemName = name
+    }
+
+    send() {
+        console.log(`Order was sent to the store. Ordered item: ${this.itemName}`)
+    }
+}
+
+class EventBus {
+    channels = {}
+
+    subscribe(channelName, cb) {
+        if (!this.channels[channelName]) {
+            this.channels[channelName] = []
+        }
+
+        this.channels[channelName].push(cb)
+    }
+
+    publish(channelName) {
+        if (!this.channels[channelName]) {
+            throw new Error('No one is subscribed to this channel')
+        }
+    }
+}
+
+const order = new Order('Apple')
+order.send()
