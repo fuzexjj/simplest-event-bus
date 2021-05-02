61f05663ebab62dc5e2b7e031a2458a34ba12d20
Kirill Koshcheev <kosheevkirill@gmail.com>
2021-05-02 02:21:20 +0200

    Added Singleton Pattern for EventBus

--- index.js
+++ index.js
@@ -1,24 +1,38 @@
-const EventBus = {
-    channels: {},
+class EventBus {
+    channels = {}
+    _instance = null
+
+    constructor() {
+        if (!EventBus._instance) {
+            EventBus._instance = this
+        }
+        return EventBus._instance
+    }
+
+    static getInstance() {
+        return this._instance
+    }
+
     subscribe(channelName, cb) {
         if (!this.channels[channelName]) {
             this.channels[channelName] = []
         }
 
         this.channels[channelName].push(cb)
-    },
+    }
+
     publish(channelName, data) {
         if (!this.channels[channelName]) {
             throw new Error('Chanel does not exist')
         }
 
         this.channels[channelName].forEach((cb) => cb(data))
-    },
+    }
 }
 
 class Mailer {
     constructor() {
-        EventBus.subscribe('order/new', this.sendEmail)
+        EventBus.getInstance().subscribe('order/new', this.sendEmail)
     }
 
     sendEmail(data) {
@@ -33,9 +47,11 @@ class Order {
 
     send() {
         console.log(`Order was sent to the store. Ordered item: ${this.itemName}`)
-        EventBus.publish('order/new', this.itemName)
+        EventBus.getInstance().publish('order/new', this.itemName)
     }
 }
+
+new EventBus()
 
 const mailer = new Mailer()
 const order = new Order('Apple')
